dd12efa9f2b7d411d8f917417f2b45ef1ce9cd10
khb8668 <khb8668@rit.edu>
2023-09-25 09:42:04 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from pybaseball import playerid_reverse_lookup
+from pybaseball import playerid_lookup
+
+df_master = pd.read_csv("Appearances.csv")
+print("Master dataframe loaded")
+
+while True:
+    team1 = input("Enter Team 1 ID: ")
+    team2 = input("Enter Team 2 ID: ")
+
+    df_t1 = df_master.loc[df_master['teamID'] == team1]
+    df_t2 = df_master.loc[df_master['teamID'] == team2]
+
+    df_both = pd.merge(df_t1, df_t2, on='playerID')
+
+    player = df_both["playerID"].iloc[-1]
+    # print(player)
+    print(playerid_reverse_lookup([player], key_type='bbref')["name_first"] + " " + playerid_reverse_lookup([player], key_type='bbref')["name_last"])
